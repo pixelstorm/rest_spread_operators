659bda34eb1533b45647a13df95cf0962407dbee
pixelstorm <dan@pixelstorm.com.au>
2021-08-25 21:10:38 +1000

    use rest operator to output all the function arguments in an array excluding the firstArg

--- rest_spread_operators.js
+++ rest_spread_operators.js
@@ -1,3 +1,7 @@
+/*
+ * use rest operator to create new object from the rest of the object values
+ */
+
 const person = {
   name: "trevor",
   age: 20,
@@ -6,3 +10,22 @@ const person = {
 
 const { age, ...newObj } = person;
 console.log(age, newObj);
+
+/*
+ * output
+ * 20 {name: "trevor", hobby: "rowing"}
+ */
+
+/*
+ * output all the function arguments in an array excluding the firstArg
+ */
+
+const someFunc = (firstArg, ...restOfArgs) => {
+  console.log(restOfArgs);
+};
+
+someFunc(1, 2, 3, 4, 5);
+
+/*
+ * output is [2, 3, 4, 5]
+ */
